8a8428ceb2fa04ac407ea1e40d8538946d8c9ab6
michael-a-tobias <michael.a.tobias@outlook.com>
2022-05-13 16:19:11 -0700

    Initial commit. Working on the prompt to reverse a string.

--- code-challenges.js
+++ code-challenges.js
@@ -21,16 +21,69 @@ const temp1 = 42
 const temp2 = 350
 const temp3 = 212
 
+// I have to create a function that takes a number. 
+// It then will compare that number whether it is equal to, less than, or greater than 212.
+// Based on how the user given number compares to 212, 
+// I am expecting an output in the form on a string that corrisponds to each of the 
+// 3 possible returns.
+
+
+const tempReader = (tempOutput) => {
+    if (tempOutput === 212) {
+        return `${tempOutput} is at boiling point.`
+    } else if (tempOutput < 212) {
+        return `${tempOutput} is below boiling point.`
+    } else {
+        return `${tempOutput} is above boiling point.`
+    }
+}
+console.log(tempReader(temp1))
+console.log(tempReader(temp2))
+console.log(tempReader(temp3))
+console.log(tempReader(999))
+
+//Output:
+// 42 is below boiling point.
+// 350 is above boiling point.
+// 212 is at boiling point.
+// 999 is above boiling point.
 
 // --------------------2) Create the code that will combine the two arrays and return the length using the variables provided below. Expected output: 10
 
 const myNumbers1 = [3, 7, 0, 36, -9]
 const myNumbers2 = [8, -7, 42, 9, 13]
 
+// I will need to take two separate arrays and combine them into a third, larger array. 
+// Once I have merged the two arrays, I will use a string method to give me 
+// a return on the longth of the third array. 
+
+var myNumbers3 = (myNumbers1.concat(myNumbers2))
+    //  a third variable = variable1.concat(variable2)
+    // .concat() is used to combine two arrays
+console.log(myNumbers3.length)
+    // I am requesting a console log of the length of this third variable 
+    // using the string method .length  
+
+// Output: 10
 
 // --------------------3) Create the code that will reverse the letters of a string using the test variable provided below. Expected output: "2202 eilrahC"
 
 const currentCohort = "Charlie 2022"
+
+// I expected to simply use console.log(currentCohort.reverse()) to have this work, but
+// upon further reading, .reverse() only works on arrays.
+// What I will have to do is to create a variable that is "Charlie 2022" as strings of
+// individual characters in an array. Then I will use the .reverse() function to 
+// reverse the array. With the array reversed, then I will bring the individual characters
+// back into a string so it will read "2202 eilrahC"
+
+var splitCohort = currentCohort.split("")
+var reverse = splitCohort.reverse()
+var reverseCohort = reverse.toString()
+
+console.log(splitCohort)
+console.log(reverse)
+console.log(reverseCohort)
 
 
 // --------------------4) Create a statement that will evaluate whether each number in an array is even or odd. Use the test variable provided below. Expected output: "odd" "even" "odd" "even" "odd" "even"
